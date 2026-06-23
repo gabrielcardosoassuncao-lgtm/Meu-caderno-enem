d243058c4c5606aeede418a9bd6fd49961a25b6e
fernando <gabriel.cardoso.assuncao@escola.pr.gov.br>
2026-06-23 08:51:38 -0300

    fiz o passo 2

--- 2Passo-RemoverBordasPaginasInteiras/remover-bordas.py
+++ 2Passo-RemoverBordasPaginasInteiras/remover-bordas.py
@@ -25,7 +25,7 @@ for nome_arquivo in os.listdir(pasta_imagens):
 
         largura, altura = imagem.size
 
-        caixa_corte = (47, 0, largura - 0, altura - 0) # ATUALIZE AQUI OS VALORES DE CORTE (esquerda, superior, direita, inferior)
+        caixa_corte = (147, 148, largura - 123, altura - 415) # ATUALIZE AQUI OS VALORES DE CORTE (esquerda, superior, direita, inferior)
         imagem_cortada = imagem.crop(caixa_corte)
 
         caminho_saida = os.path.join(pasta_saida, nome_arquivo)
